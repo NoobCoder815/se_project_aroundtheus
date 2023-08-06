d09eaefd3051702b0a15a7693f0abd0b147790f9
Richard Jordan <rjordan048@gmail.com>
2023-08-06 16:26:32 -0400

    add gallery and edit functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,15 +24,48 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const modal = document.querySelector("#profile-edit-modal");
 const profileCloseBtn = document.querySelector("#modal-close-button");
+const profileEditForm = document.querySelector(".modal__form");
+const inputName = profileEditForm.querySelector("#profile-name");
+const inputJob = profileEditForm.querySelector("#profile-description");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
+const profileTemplate = document.querySelector("#gallery-template").content;
+const profileGallery = document.querySelector(".gallery__cards");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  modal.classList.remove("modal_opened");
+}
+
+function getCardElement(data) {
+  const cardElement = profileTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  cardTitle.textContent = data.name;
+  cardTitle.alt = data.name;
+  cardImage.src = data.link;
+  return cardElement;
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardData = initialCards[i];
+  const cardElement = getCardElement(cardData);
+  profileGallery.append(cardElement);
+}
 
 profileEditBtn.addEventListener("click", function () {
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
   modal.classList.add("modal_opened");
 });
 
 profileCloseBtn.addEventListener("click", () => {
   modal.classList.remove("modal_opened");
 });
+
+profileEditForm.addEventListener("submit", handleProfileFormSubmit);

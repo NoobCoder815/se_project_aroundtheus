58ef11713ae108f895ebd91aba63d9e538b47e8b
Richard Jordan <rjordan048@gmail.com>
2023-10-11 14:15:44 -0400

    feat/ integrate webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-// webpack.config.js
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // connect plugin
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // connect plugin
+// connect mini-css-extract-plugin to the project
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -10,26 +10,31 @@ module.exports = {
     main: "./src/pages/index.js",
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), // you could name this anything you want, but let's stick to 'dist'
+    filename: "main.js", // you could also name this anything you want, but let's stick to 'main.js'
     publicPath: "",
   },
   target: ["web", "es5"],
   stats: "errors-only",
   mode: "development",
   devServer: {
-    static: path.resolve(__dirname, "dist"),
-    compress: true,
-    port: 8080,
-    open: true,
+    static: path.resolve(__dirname, "./dist"), // specifies a folder from where to serve the application and its contents
+    compress: true, // this will speed up file loading in development mode
+    port: 8080, // will open your site at localhost:8080 (you can use another port)
+    open: true, // site will open automatically in the browser after executing npm run dev
     liveReload: true,
     hot: false,
   },
   module: {
     rules: [
+      // this is an array of rules
+      // add an object containing rules for Babel to it
       {
+        // a regular expression that searches for all js files
         test: /\.js$/,
+        // all files must be processed by babel-loader
         loader: "babel-loader",
+        // exclude the node_modules folder, we don't need to process files in it
         exclude: "/node_modules/",
       },
       {
@@ -38,10 +43,15 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
+            // add an options object
+            options: { importLoaders: 1 },
           },
+          // add postcss-loader
+          "postcss-loader",
         ],
       },
       {
+        // add the rule for processing files
         test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
         type: "asset/resource",
       },
@@ -49,9 +59,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./src/index.html", // path to our index.html file
+      favicon: "./src/images/favicon.ico",
     }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(), // use plugin
+    new MiniCssExtractPlugin(), // connect the plugin for merging CSS files
   ],
 };
